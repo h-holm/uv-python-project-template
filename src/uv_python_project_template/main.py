a91105d1a372222a5e3e33b8c83ac8bb84d2035c
Henrik Holm <holmhenrik93@gmail.com>
2025-12-26 10:01:31 +0100

    Update comment

--- src/uv_python_project_template/main.py
+++ src/uv_python_project_template/main.py
@@ -13,8 +13,8 @@ from loguru import logger
 # https://packaging.python.org/en/latest/discussions/src-layout-vs-flat-layout/#running-a-command-line-interface-from-source-with-src-layout
 if not __package__:  # pragma: no cover
     # Add the grandparent directory to `sys.path` to enable running all of the following:
-    # - `python path/to/this/repo/src/uv_python_project_template/main.py`   from anywhere,
-    # - `hatch run python src/uv_python_project_template/main.py`           from the root of this repo,
+    # - `python ${PATH_TO_THIS_REPO}/src/uv_python_project_template/main.py`  from anywhere,
+    # - `uv run python src/uv_python_project_template/main.py`                from the root of this repo.
     sys.path.insert(0, str(Path(__file__).parents[1]))
 
 
